9a5c0697e84a79174cf869030cec99a291955d1a
sophiacharteris <sophiacharteris@gmail.com>
2025-03-13 17:18:54 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="container-fluid text-center">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
+            <p>JOSH WHITKIN</p>
+            </div>
+            <div class="col-md-6">
+                <p>Terms and Conditions</p>
+                </div>
+    
+
+
+
+        </div> <!-- column-->
+    </div> <!-- row -->
+</footer>
+<?php wp_footer(); ?>
+</html>
